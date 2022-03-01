d4cd0c9f1d5122ae6608c676c45685140ddf4c15
lavishal <ajt33@uw.edu>
2022-03-01 11:53:09 -0800

    added packs and apt get

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,44 @@
-import express from 'express';
+
+import express from 'express'
+import Post from '../db.js'
 var router = express.Router();
 
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async function(req, res, next) {
+  const allPosts = await Post.find()
+  res.json({ posts:allPosts})
 });
+
+router.post('/', async function(req, res) {
+  try {
+   const {placeName, area, size, distanceAway, price, description, leasingterm} = req.body;
+    const newPost = new Post({ placeName, area, size, distanceAway, price, description, leasingterm});
+    await newPost.save();
+    if(newPost) {
+    return res.json({
+      message: 'New post created',
+      data: newPost
+    })
+    }
+  }catch(err) {
+    return res.status(400).json({
+      message: 'Could not create a new post'
+    })
+  }
+})
+
+router.get('/', async function(req, res) {
+  try {
+   
+
+
+    
+  }catch(err) {
+    return res.status(400).json({
+      message: 'Could not create a new post'
+    })
+  }
+})
 
 export default router;

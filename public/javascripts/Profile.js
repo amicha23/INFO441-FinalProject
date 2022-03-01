2d8db9401f977ac58174c70830750cb5572f3a43
Andrew Michaels <amicha@uw.edu>
2022-03-01 11:42:51 -0800

    push user endpoints and js but commented out

---
+++ public/javascripts/Profile.js
@@ -0,0 +1,71 @@
+// async function init(){
+// }
+
+// async function saveUser(){
+//   const urlParams = new URLSearchParams(window.location.search);
+//   const username = urlParams.get('user');
+//   try {
+//       let response = await fetch(`/users`, {
+//           method: "POST",
+//           body: JSON.stringify({username: username}),
+//           headers: {'Content-Type': 'application/json'}
+//       })
+//       let responseJSON = await response.json();
+//       if (responseJSON.status == "error"){
+//           console.log("error:" + responseJSON.error);
+//       }
+//       return responseJSON;
+//   } catch(error){
+//       console.log("error:" + error);
+//   }
+// }
+
+// async function loadUser(){
+//   const urlParams = new URLSearchParams(window.location.search);
+//   const username = urlParams.get('user');
+//   try {
+//   let response = await fetch(`/users?username=${encodeURIComponent(username)}`);
+//   let responseJSON = await response.json();
+//   let newHTML = `<a href=${responseJSON}>${responseJSON}</a>`
+//   // load newHTML
+//   // document.getElementById("user_info_div").innerHTML = newHTML;
+//   } catch(error) {
+//     console.log(error);
+//   }
+// }
+
+// // Save an apartment for a user
+// async function saveApt(apt){
+//   // load apt name into 'apt' parameter in html
+//   try{
+//       let response = await fetch(`/users/saveApt`,
+//           { method: "POST", body: JSON.stringify({apt: apt}), headers: {'Content-Type': 'application/json'}})
+//       let responesJSON = await response.json();
+//       if(responesJSON.status == "error"){
+//           console.log("error:" + responesJSON.error);
+//       }else{
+//           loadUser();
+//       }
+//       return responesJSON;
+//   }catch(error){
+//       console.log("error:" + error);
+//   }
+// }
+
+// // Remove an apartment for a user
+// async function unsaveApt(apt){
+//   // load apt name into 'apt' parameter in html
+//   try{
+//       let response = await fetch(`/users/unsaveApt`,
+//           { method: "POST", body: JSON.stringify({apt: apt}), headers: {'Content-Type': 'application/json'}})
+//       let responesJSON = await response.json();
+//       if(responesJSON.status == "error"){
+//           console.log("error:" + responesJSON.error);
+//       }else{
+//           loadPosts();
+//       }
+//       return responesJSON;
+//   }catch(error){
+//       console.log("error:" + error);
+//   }
+// }
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,87 @@
-import express from 'express';
-var router = express.Router();
+// import express from 'express';
+// var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// /* GET user info. */
+// router.get('/', async function(req, res, next) {
+//   let session = req.session;
+//   try {
+//     let findUser = session.account.username;
+//     let userInfo = await req.db.User.find().where('username').in(findUser).exec(); //change to db name
+//     let allapt = userInfo[0].saved;
+//     let aptInfo = []
+//     for (let i = 0; i < allapt.length; i++) {
+//       let apt = await req.db.Apartment.find().where('placeName').in(allapt[i]).exec();
+//       aptInfo.push(apt);
+//     }
+//     res.json(aptInfo);
+//   } catch(error) {
+//     res.json({"status": "error", "error": error})
+//   }
+// });
 
-export default router;
+
+// // Save User -> Create Account button?
+// router.post('/', async function(req, res, next) {
+//   let session = req.session;
+//   try {
+//     const User = new req.db.UserInfo({
+//       username: session.account.username,
+//       saved: [String]
+//     })
+//     await User.save();
+//     res.json({"status": "success"});
+//   } catch(error) {
+//     res.json({"status": "error", "error": error})
+//   }
+// });
+
+// // Add apt to saved -> Add save button
+// router.post('/saveApt', async function(req, res, next) {
+//   let session = req.session;
+//   if (session.isAuthenticated) {
+//     try {
+//       let newSave = req.query.apt; // or req.body.apt;
+//       let findUser = session.account.username;
+//       let userInfo = await req.db.User.find().where('username').in(findUser).exec(); //change to db name
+//       // let allapt = userInfo[0].saved;
+//       // load post from the database
+
+//       if (!userInfo[0].saved.includes(newSave)) {
+//         //Include currently logged in user
+//         userInfo[0].saved.push(newSave);
+//         await userInfo[0].save();
+//       }
+//       res.json({"status": "success"});
+//     } catch(error) {
+//       res.json({"status": "error", "error": error})
+//     }
+//   } else {
+//     res.json({"status": "error", "error": "not logged in"})
+//   }
+// })
+
+// // Remove saved apt
+// router.post('/unsaveApt', async function(req, res, next) {
+//   let session = req.session;
+//   if (session.isAuthenticated) {
+//     try {
+//       let findApt = req.body.apt; // or query param
+//       let findUser = session.account.username;
+//       let userInfo = await req.db.User.find().where('username').in(findUser).exec(); //change to db name
+//         if (userInfo[0].saved.includes(findApt)) {
+//           //remove apt from saved
+//           userInfo[0].saved = userInfo[0].saved.filter((element) => {
+//             return element !== findApt;
+//           })
+//           await userInfo[0].save();
+//         }
+//         res.json({"status": "success"});
+//     } catch (error) {
+//       res.json({"status": "error", "error": error})
+//     }
+//   } else {
+//     res.json({"status": "error", "error": "not logged in"})
+//   }
+// })
+
+// export default router;
